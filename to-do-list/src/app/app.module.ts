171b0f3ed852e153cf5b9256734bf163a5346364
Daniel Santos <danielgdt88@hotmail.com>
2024-11-08 23:38:15 -0300

    feat: crio componente input

--- to-do-list/src/app/app.module.ts
+++ to-do-list/src/app/app.module.ts
@@ -6,16 +6,22 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonHeaderComponent } from './components/button-header/button-header.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { InputComponent } from './components/input/input/input.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonHeaderComponent
+    ButtonHeaderComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [
     provideAnimationsAsync()

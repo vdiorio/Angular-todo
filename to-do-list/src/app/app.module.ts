d6a4d4592f35b533e9de2728c9853f9946394143
Daniel Santos <danielgdt88@hotmail.com>
2024-11-08 23:22:51 -0300

    feat: adiciono package do material UI e transition do bg

--- to-do-list/src/app/app.module.ts
+++ to-do-list/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonHeaderComponent } from './components/button-header/button-header.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { ButtonHeaderComponent } from './components/button-header/button-header.
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
